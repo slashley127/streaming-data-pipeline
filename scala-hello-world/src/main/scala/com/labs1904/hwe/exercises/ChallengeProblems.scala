3510785b6ccaeea4296d671c058e5b469ca77aae
unknown <slashley127@gmail.com>
2023-04-09 21:50:29 -0500

    scala challenge problems complete, all tests pass

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
@@ -15,6 +15,9 @@ object ChallengeProblems {
   Params - None
   Returns - String
    */
+def helloWorld() = {
+  "Hello World!"
+}
 
   /*
   3. Write a function that takes in a list and returns the total size of the list
@@ -22,6 +25,9 @@ object ChallengeProblems {
   Params - List[Int]
   Returns - Int
    */
+  def listSize(input: List[Int]): Int = {
+    input.size
+  }
 
   /*
   4. Write a function that takes in an int and adds an int that you create within the function and returns the addition of the two together
@@ -29,7 +35,10 @@ object ChallengeProblems {
   Params - Int
   Returns - Int
    */
-
+  def sumInts(input: Int): Int={
+    val num: Int = 25
+    input+num
+  }
 
   /*
    5. Write a function that takes in a list of strings, and return a list of strings where every letter is capitalized
@@ -37,24 +46,40 @@ object ChallengeProblems {
    Params - List[String]
    Returns - List[String]
 */
+  def upper(input: List[String]): List[String]={
+    input.map(input => input.toUpperCase())
+  }
 
   /*
   6. Write a function that returns a new list, where only elements of the list passed in that are 0 or positive numbers are kept.
   Params - List[Int]
   Returns - List[Int]
    */
+  def filterNegatives(input: List[Int]): List[Int]={
+    input.filter(_>=0)
+  }
 
   /*
   7. Returns a new list, where only the elements passed in containing "car" are kept to the new list.
   Params - List[String]
   Returns - List[String]
  */
+  def containsCar(input: List[String]): List[String]={
+    input.filter(_.contains("car"))
+  }
 
   /*
     8. Returns the sum of all numbers passed in.
     Params - List[Int]
     Returns - Int
    */
+  def sumList(input: List[Int]): Int={
+    var sum = 0
+    for (x <- input){
+      sum += x
+    }
+    sum
+  }
 
   /*
   9. Write a function that takes in an integer with a cats age, and return the human age equivalent.
@@ -62,6 +87,9 @@ object ChallengeProblems {
     Params - Int
     Returns - Int
    */
+  def catsAge(input: Int): Int={
+    input*4
+  }
 
   /*
   10. Same question as #9, but this time you are given a Option[Int]
@@ -71,17 +99,31 @@ object ChallengeProblems {
     -Params - Option[Int]
     -Returns - Option[Int]
  */
+  def catsAgeOption(input: Option[Int]): Option[Int]={
+    if(input.isDefined) Some(input.get*4)
+    else None
+  }
 
   /*
   11. Write a function that takes in a list of ints, and return the minimum of the ints provided
   Params - List
   Returns - Int
    */
+  def minimum(input: List[Int]): Int={
+    input.min
+  }
 
   /*
   12. Same as question 11, but this time you are given a list of Option[Ints], returns the minimum of the Ints provided.
   If no ints are provided, return None.
  */
+
+  def minimumOption(input: List[Option[Int]]): Option[Int]={
+    val onlyInts = input.flatten
+    if(onlyInts.isEmpty){
+      None
+    } else Some(onlyInts.min)
+  }
 
 
 
