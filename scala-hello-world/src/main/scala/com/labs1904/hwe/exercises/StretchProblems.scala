95bb8b583a26a2029d45061ed37b0fa7ba45b5aa
Slash <slashley127@gmail.com>
2023-04-17 15:51:58 -0500

    scala challenge problems

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/StretchProblems.scala
@@ -5,14 +5,16 @@ object StretchProblems {
   /*
   Checks if a string is palindrome.
  */
-  def isPalindrome(s: String): Boolean = ???
+  def isPalindrome(s: String): Boolean = {
+   s.reverse == s
+  }
 
   /*
 For a given number, return the next largest number that can be created by rearranging that number's digits.
 If no larger number can be created, return -1
  */
   def getNextBiggestNumber(i: Integer): Int = {
-    //TODO: Implement me!
+
     0
   }
 
