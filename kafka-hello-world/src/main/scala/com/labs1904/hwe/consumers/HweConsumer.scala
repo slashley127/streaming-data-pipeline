d594ef6b8c899607f0dca1cba51f899ffec52659
Slash <slashley127@gmail.com>
2023-04-17 20:49:06 -0500

    Kafka studio - almost complete

--- kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/HweConsumer.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/HweConsumer.scala
@@ -10,6 +10,9 @@ import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.util.Arrays
 
+case class RawUser(id: Int, username: String, name: String, email: String, date: String)
+case class EnrichedUser(rawUser: RawUser, numberAsWord: String, hweDeveloper: String)
+
 object HweConsumer {
   private val logger = LoggerFactory.getLogger(getClass)
 
@@ -39,9 +42,17 @@ object HweConsumer {
       val records: ConsumerRecords[String, String] = consumer.poll(duration)
 
       records.forEach((record: ConsumerRecord[String, String]) => {
+
         // Retrieve the message from each record
-        val message = record.value()
-        logger.info(s"Message Received: $message")
+        val message: Array[String] = record.value().split("\t")
+        val user = RawUser(message(0).toInt, message(1), message(2), message(3), message(4) )
+        val utilMethod = Util.mapNumberToWord(user.id)
+        val hweDeveloper = "Slash"
+        val enrichedUser= EnrichedUser(user, utilMethod, hweDeveloper)
+
+        val fields: List[String] = List(user.date, user.name, user.email, user.username, user.id.toString, utilMethod, hweDeveloper)
+        val output: String = fields.mkString(",")
+        logger.info(s"Message Received: $output")
         // TODO: Add business logic here!
 
       })
--- kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
@@ -4,10 +4,10 @@ import java.nio.file.Paths
 
 object Constants {
   // TODO: this is configured to use kafka running locally, change it to your cluster
-  val BOOTSTRAP_SERVER : String = "b-3-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-2-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-1-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196"
+  val BOOTSTRAP_SERVER : String = "CHANGE ME"
   val DEFAULT_TOPIC: String = "connection-test"
-  val USERNAME: String = "1904labs"
-  val PASSWORD: String = "1904labs"
+  val USERNAME: String = "change me"
+  val PASSWORD: String = "change me"
   //Use this for Windows
   val TRUST_STORE: String = {
     val url = getClass.getResource("/kafka.client.truststore.jks")
