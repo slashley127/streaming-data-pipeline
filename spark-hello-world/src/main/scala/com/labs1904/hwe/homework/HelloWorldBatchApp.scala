5089292bdb457f67d5c627efe43abbd8f6ebc9c8
unknown <slashley127@gmail.com>
2023-04-23 11:35:12 -0500

    completed second part of homework

--- spark-hello-world/src/main/scala/com/labs1904/hwe/homework/HelloWorldBatchApp.scala
+++ spark-hello-world/src/main/scala/com/labs1904/hwe/homework/HelloWorldBatchApp.scala
@@ -11,12 +11,15 @@ object HelloWorldBatchApp {
     try {
       logger.info(s"$jobName starting...")
       //TODO: What is a spark session - Why do we need this line?
+      //A spark session is how we connect the dataset to Spark. This is what says the programs needs to create a new Spark session.
       val spark = SparkSession.builder()
         .appName(jobName)
         .config("spark.sql.shuffle.partitions", "3")
         //TODO- What is local[*] doing here?
+        //This directs to the cluster manager, which, for this project is our local machines. Can direct to YARN for larger projects connected through Hadoop.
         .master("local[*]")
         //TODO- What does Get or Create do?
+        //getOrCreate() gets an existing Spark session or creates a new one if there is no existing one based on options set in the builder (line 15)
         .getOrCreate()
 
       import spark.implicits._
@@ -25,6 +28,7 @@ object HelloWorldBatchApp {
       sentences.printSchema
       // display some data in the console, useful for debugging
       //TODO- Make sure this runs successfully
+      //It Works!
       sentences.show(truncate = false)
     } catch {
       case e: Exception => logger.error(s"$jobName error in main", e)
