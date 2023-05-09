0a5d68018f2718c0933f96512c2d7191a0f4dc0c
Slash <slashley127@gmail.com>
2023-05-08 19:50:04 -0500

    hbase lab challenge #2 complete

--- hbase-hello-world/src/main/scala/com/labs1904/hwe/MyApp.scala
+++ hbase-hello-world/src/main/scala/com/labs1904/hwe/MyApp.scala
@@ -1,7 +1,7 @@
 package com.labs1904.hwe
 
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get}
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get, Put}
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.logging.log4j.{LogManager, Logger}
 
@@ -17,15 +17,42 @@ object MyApp {
       connection = ConnectionFactory.createConnection(conf)
       // Example code... change me
       val table = connection.getTable(TableName.valueOf(("slashley:users")))
-      val get = new Get(Bytes.toBytes("10000001"))
-      get.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("mail"))
+
+      val put = new Put(Bytes.toBytes("99"))
+      put.addColumn(
+        Bytes.toBytes("f1"),
+        Bytes.toBytes("username"),
+        Bytes.toBytes("DE-HWE")
+      )
+      put.addColumn(
+        Bytes.toBytes("f1"),
+        Bytes.toBytes("name"),
+        Bytes.toBytes("The Panther")
+      )
+      put.addColumn(
+        Bytes.toBytes("f1"),
+        Bytes.toBytes("sex"),
+        Bytes.toBytes("F")
+      )
+      put.addColumn(
+        Bytes.toBytes("f1"),
+        Bytes.toBytes("favorite_color"),
+        Bytes.toBytes("pink")
+      )
+      table.put(put)
+
+      val get = new Get(Bytes.toBytes("99"))
       val result = table.get(get)
-      val mail =
-        Bytes.toString(
-          result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
+
+      //val get = new Get(Bytes.toBytes("10000001"))
+      //get.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("mail"))
+      //val result = table.get(get)
+      //val mail =
+        //Bytes.toString(
+          //result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail")))
 
 
-      logger.debug(mail) //logger
+      logger.debug(result) //logger is what actually prints to console
     } catch {
       case e: Exception => logger.error("Error in main", e)
     } finally {
